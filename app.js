cce9a7dc1ce447f3a5c5e985f38b7b740c2a389b
Robert Souza <robert.souza@mundiale.com.br>
2021-04-01 17:02:54 -0300

    add to cardinal numbers

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
+var writtenNumber = require('written-number');
 var stringSimilarity = require("string-similarity");
 const MinimumSimilarityScore = 0.6;
+const map = {
+    '-': ' ',
+    '-': '_',
+    'a': 'á|à|ã|â|ä|À|Á|Ã|Â|Ä',
+    'e': 'é|è|ê|ë|É|È|Ê|Ë',
+    'i': 'í|ì|î|ï|Í|Ì|Î|Ï',
+    'o': 'ó|ò|ô|õ|ö|Ó|Ò|Ô|Õ|Ö',
+    'u': 'ú|ù|û|ü|Ú|Ù|Û|Ü',
+    'c': 'ç|Ç',
+    'n': 'ñ|Ñ'
+};
 
 function removeAccentuation(str) {
-    var map = {
-        '-': ' ',
-        '-': '_',
-        'a': 'á|à|ã|â|ä|À|Á|Ã|Â|Ä',
-        'e': 'é|è|ê|ë|É|È|Ê|Ë',
-        'i': 'í|ì|î|ï|Í|Ì|Î|Ï',
-        'o': 'ó|ò|ô|õ|ö|Ó|Ò|Ô|Õ|Ö',
-        'u': 'ú|ù|û|ü|Ú|Ù|Û|Ü',
-        'c': 'ç|Ç',
-        'n': 'ñ|Ñ'
-    };
+
 
     for (var pattern in map) {
         str = str.replace(new RegExp(map[pattern], 'g'), pattern);
@@ -42,41 +44,66 @@ function clearInput(str) {
 
 function findByint(input, options) {
     input = input.replace(/^\D+/g, '');
-    var pos = parseInt(input) - 1;
+    var position = parseInt(input) - 1;
     let response = null;
-    if (pos < options.length) {
+    if (position < options.length) {
         response = {
             rating: 1,
-            option: options[pos].Text,
-            index: pos
+            option: options[position].Text,
+            index: position
         }
     }
     return response;
 }
 
-function findBestMatch(input, options) {
+function addCardinalAndTextOptions(options) {
+    options.forEach((element) => {
+        var option = writtenNumber((parseInt(element.Index) + 1), { lang: 'pt' });
+        element.Synonyms.push(option)
+    });
+    return options;
+}
+
+function findBestMatchBySynonyms(input, options) {
+    var all_matches = [];
+    options.forEach((element) => {
+        let vectTextS = element.Synonyms.map((option) => {
+            return clearInput(option);
+        })
+        all_matches.push(stringSimilarity.findBestMatch(input, vectTextS));
+    });
+    var matches = all_matches.reduce(function (p, v) {
+        return (p.bestMatch.rating > v.bestMatch.rating ? p : v);
+    });
+    let index = all_matches.indexOf(matches);
+    let selectedOption = options[index];
+    selectedOption.rating = matches.bestMatch.rating
+    return selectedOption;
+}
+
+function findBestMatch(input, options, minimumScore) {
+    options = addCardinalAndTextOptions(options)
     let vectText = options.map((option) => {
         return clearInput(option.Text);
     })
+
     var matches = stringSimilarity.findBestMatch(input, vectText);
-    let ops = options[matches.bestMatchIndex];
-
-    if (matches.bestMatch.rating <= MinimumSimilarityScore && options[matches.bestMatchIndex].Synonyms.length > 0) {
-        let vectTextS = options[matches.bestMatchIndex].Synonyms.map((option) => {
-            return clearInput(option);
-        })
-        matches = stringSimilarity.findBestMatch(input, vectTextS);
-    }
-    let ret = {
-        rating: matches.bestMatch.rating.toFixed(2),
-        option: ops.Text,
-        index: ops.Index
+    let selectedOption = options[matches.bestMatchIndex];
+    selectedOption.rating = matches.bestMatch.rating
+    if ( selectedOption.rating <= minimumScore) {
+        selectedOption = findBestMatchBySynonyms(input, options)
     }
 
-    if (matches.bestMatch.rating <= MinimumSimilarityScore && parseInt(input) != -1) {
-        ret = findByint(input, options)
+    let response = {
+        rating:  selectedOption.rating.toFixed(2),
+        option: selectedOption.Text,
+        index: selectedOption.Index
     }
-    return ret;
+
+    if ( selectedOption.rating <= minimumScore && parseInt(input) != -1) {
+        response = findByint(input, options)
+    }
+    return response;
 }
 
 const http = require('http');
@@ -93,10 +120,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 // A sample route
 app.get('/', (req, res) => res.send('Hello World!'))
-app.post('/', (req, res) => {
+app.post('/', async (req, res) => {
     input = req.body.input;
     options = req.body.options;
-    let resp = findBestMatch(clearInput(input), options);
+    minimumScore = req.body.minimumScore ? req.body.minimumScore : MinimumSimilarityScore
+   
+    let resp = await findBestMatch(clearInput(input), options, minimumScore);
     if (resp == null) {
         res.sendStatus(204)
     } else {
